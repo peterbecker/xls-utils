f88d58c7d3a7e19016a19934361ef24ac4123450
Peter Becker <peter@peterbecker.de>
2019-03-17 17:15:51 +1000

    Removed all the implicit casting.
    It's not good Kotlin API, callers should really use `where` to express their expecations.

--- kotlin-xls/src/main/kotlin/de/peterbecker/xls/Sheet.kt
+++ kotlin-xls/src/main/kotlin/de/peterbecker/xls/Sheet.kt
@@ -3,7 +3,3 @@ package de.peterbecker.xls
 import org.apache.poi.ss.usermodel.Sheet
 
 fun Sheet.getValueAt(row: Int, col: Int) = this.getRow(row)?.getCell(col)?.getValue()
-
-fun Sheet.getBooleanValueAt(row: Int, col: Int) = this.getRow(row)?.getCell(col)?.getValue() as Boolean?
-fun Sheet.getDoubleValueAt(row: Int, col: Int) = this.getRow(row)?.getCell(col)?.getValue() as Double?
-fun Sheet.getStringValueAt(row: Int, col: Int) = this.getRow(row)?.getCell(col)?.getValue() as String?
--- kotlin-xls/src/test/kotlin/de/peterbecker/xls/CellValueTests.kt
+++ kotlin-xls/src/test/kotlin/de/peterbecker/xls/CellValueTests.kt
@@ -1,5 +1,6 @@
 package de.peterbecker.xls
 
+import org.apache.poi.ss.usermodel.Sheet
 import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.Offset.offset
@@ -13,62 +14,17 @@ class CellValueTests {
         val sheet = workbook.getSheet("basic")
         assertThat(sheet.getDoubleValueAt(0, 0)).isCloseTo(1.0, offset(0.000001))
         assertThat(sheet.getValueAt(0, 1)).isEqualTo(true)
-        assertThat(sheet.getBooleanValueAt(0, 1)).isEqualTo(true)
         assertThat(sheet.getValueAt(0, 2)).isEqualTo("Hello World")
-        assertThat(sheet.getStringValueAt(0, 2)).isEqualTo("Hello World")
         assertThat(sheet.getDoubleValueAt(0, 3)).isCloseTo(5.67, offset(0.000001))
     }
+
+    private fun Sheet.getDoubleValueAt(row: Int, col: Int) = this.getValueAt(row, col) as Double?
 
     @Test
     fun testMissingValues() {
         val workbook = loadWorkbook("values")
         val sheet = workbook.getSheet("basic")
         assertThat(sheet.getValueAt(100, 100)).isNull()
-        assertThat(sheet.getBooleanValueAt(100, 100)).isNull()
-        assertThat(sheet.getDoubleValueAt(100, 100)).isNull()
-        assertThat(sheet.getStringValueAt(100, 100)).isNull()
-    }
-
-    @Test
-    fun testTypeMismatches() {
-        val workbook = loadWorkbook("values")
-        val sheet = workbook.getSheet("basic")
-        try {
-            sheet.getBooleanValueAt(0, 0)
-            fail("Should have thrown exception")
-        } catch (e: ClassCastException) {
-            // expected
-        }
-        try {
-            sheet.getStringValueAt(0, 0)
-            fail("Should have thrown exception")
-        } catch (e: ClassCastException) {
-            // expected
-        }
-        try {
-            sheet.getDoubleValueAt(0, 1)
-            fail("Should have thrown exception")
-        } catch (e: ClassCastException) {
-            // expected
-        }
-        try {
-            sheet.getStringValueAt(0, 1)
-            fail("Should have thrown exception")
-        } catch (e: ClassCastException) {
-            // expected
-        }
-        try {
-            sheet.getBooleanValueAt(0, 2)
-            fail("Should have thrown exception")
-        } catch (e: ClassCastException) {
-            // expected
-        }
-        try {
-            sheet.getDoubleValueAt(0, 2)
-            fail("Should have thrown exception")
-        } catch (e: ClassCastException) {
-            // expected
-        }
     }
 
     private fun loadWorkbook(name: String) =
